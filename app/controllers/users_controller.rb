4bcba1f70f87a712e6fbe1d7c0dbabdafda68143
Michihiro Adachi <madachi789@gmail.com>
2017-03-29 10:49:55 +0000

    user-profile work done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
   end
-  
+
+  # 表示する用 → 更新する用
+  # new → create
+  # edit → update
+
   def new
     @user = User.new
   end
@@ -18,10 +22,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+        redirect_to root_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:success] = 'Update success!'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :profile, :password, :password_confirmation)
   end
 end
